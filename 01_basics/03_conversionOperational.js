16c72e42caadc44eb704d613175c4e7d1fe89cd3
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-07 12:07:56 +0000

    conversion operational

---
+++ 01_basics/03_conversionOperational.js
@@ -0,0 +1,27 @@
+let score = 18 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valuInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"= 33
+// "33abc" = Nan
+// true=1;false=0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+ 
