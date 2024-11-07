69761f9eac9e1675c08d8060c08e0582a2bcfb8a
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-07 11:34:27 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; 
+// treat all the js code as newer version
+// alert("hello") we are using node js not the browser
+
+// console.log(3
+//     +3); but this is not the correct practise to do so
+// because code writing matters.
+console.log("hello");
+
+let name = "aryan"
+let age = 18
+let isLoggedIn = false 
+
+//  number =>2 to the power 53
+// bignit 
+// string =>""
+// boolean =>true/false 
+// null =>standalaone value
+// undefined =>
+// symbol =>unique
+
+
+// object 
+
+console.log(typeof null);// Object
+console.log(typeof undefined);// undefined value
+
+
