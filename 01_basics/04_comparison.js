401cec92a2b84e1f355fdb445fc4f3aabf01b1ab
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-10 14:23:34 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,35 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+/*the answer to these questions will be 
+coming in the form of true and false */
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);         
+ /*first is false 
+ second is also false 
+ third is also false 
+
+Comparison convert null to a number treating it as zero so we can 
+see the answer for the first is false but for the third one it is 
+coming true
+*/
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null < 1); 
+/*
+from teh forth one we can clearly see this by doing 
+investigation
+
+*/
+
+// === compare datatype also in which type the value is,
+// written
+
+console.log("2" === 2);
+
+
