f8de590e74e2458c4f6c9503fee351d3caa58d23
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-10 11:44:40 +0000

    hi

---
+++ 01_basics/03_conversionOperational.js
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+"33" => 33 
+"33abc" => Nan
+true =>1  and false=>0
+*/
+
+// let isLoggedIn = 1 
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true
+"" => false 
+"aryan"=> true 
+
+
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+// ******************************Operations*********************
+let value = 3
+let negvalue = -value 
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log(1 + 2 + "2");
+
+console.log(3 + 4 * 5 %  3);
+
+
+
+
+
