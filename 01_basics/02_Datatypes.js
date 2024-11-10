eb0d370ad3e8604242f3e79360eee87b41b2cba5
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-10 09:31:01 +0000

    Reset

--- 01_basics/02_Datatypes.js
+++ 01_basics/02_Datatypes.js
@@ -1,28 +1,35 @@
-"use strict"; 
-// treat all the js code as newer version
-// alert("hello") we are using node js not the browser
+ /* "use strict";  now your code will treat like 
+                  newer version of js */
 
-// console.log(3
-//     +3); but this is not the correct practise to do so
-// because code writing matters.
-console.log("hello");
+/* alert( 3 + 3 )
+we are using node js not the browser*/
+
+console.log( 3 
+    + 3 );/*code rediablity should be maintained*/
+console.log("Hitesh");
 
 let name = "aryan"
 let age = 18
 let isLoggedIn = false 
 
-//  number =>2 to the power 53
-// bignit 
-// string =>""
-// boolean =>true/false 
-// null =>standalaone value
-// undefined =>
-// symbol =>unique
+/*
+number should be in the range of 2 to the power 53
+bigint is used for very big number in trading and many other big websites
+string = "   "
+boolean = true/false 
+null = standalone value 
+undefined = abhi value asign nhi hui hai
+symbol "uniques ness ke liye kaam ata hai"
+object 
+*/
 
-
-// object 
-
-console.log(typeof null);// Object
-console.log(typeof undefined);// undefined value
-
+console.log(typeof null);
+/* 
+when we look upto the 
+console.log(typeof null); it shows the output as objecct
+*/
+console.log(typeof undefined);
+/*
+but when we look upto the typeof undefined it shows undefined
+*/
 
