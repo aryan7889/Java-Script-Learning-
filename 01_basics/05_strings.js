1e812c9c075f526fc1eee2900726d2958248a83d
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-12 05:34:25 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,8 +1,48 @@
 const name = "hitesh"
-const repoCount = 50 
+const repocount = 50 
 
-// console.log(name + repoCount + " value");This is outdated syntax
+// console.log(name + repocount + " Value");
+/*but this is outdated method to write 
+*/
+/* so we have new method to write it by the use of backticks
+string intepolation by the use of dollar sign */
 
-console.log(`hello my name is ${name} and repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repocount is ${repocount} `);
+
+
+
+/*
+NEW METHOD TO DECLARE STRING
+
+
+
+*/
+const gamename = new String('hitesh-hc')
+console.log(gamename[0]);/* Method to access the key of the value pair */
+
+console.log(gamename.__proto__);/* one of the easiest prototype */
+
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());/* now it doesnt change our 
+original value because of the heap memory (non primitive)*/
+
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('t'));
+
+
+const newString = gamename.substring(0, 4);
+console.log(newString);
+
+// have to read documentation on mdn
+
+
+
+
+
+
+
+
+
 
 
