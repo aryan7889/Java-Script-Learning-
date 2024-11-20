824c0400837dd23da3e8eda288fd85a07d0115b5
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-20 13:38:44 +0000

    arrays

---
+++ 02_arrays/01_arrays.js
@@ -0,0 +1,71 @@
+
+const myArr = [0, 1, 2, 3, 4, 5]
+/*Array is some what storing a collection of multiple items under a single 
+variables name as we have doen in orevious line , and has members
+for performing common tasks*/
+
+console.log(myArr[0]);/*access karena ka tarika array me aise hi hota hai and value start hoti hai
+                        zero se, ye array ka feature hai */
+
+const myHeros = ["shaktiman", "nagraj"]/*there are multiple ways to declare arrya*/
+
+const myArr2 = new Array(1, 2, 3, 4)/*one more way to add arrya by the use of keyward*/
+
+// Array methods 
+
+myArr.push(8)        /*This is the method to push a value or a object inside
+                       array so here output will be 0 1 2 3 4 5 and finally the value 
+                       which we have just push which is 8*/
+myArr.push(7)               
+
+myArr.pop()         /*this will help us to remove last value from the 
+                      array*/
+
+myArr.unshift(9)    /*this will hepl us to push new value which is
+                    "9" and it will show us the value in the very 
+                    begning of the array which is "0" value.*/
+
+myArr.shift()       /*help us to remove the value that we have pushed
+                      in the array or the zeroth value in the original array
+                       with the help of unshift function*/
+
+console.log(myArr.includes(9));/*
+                                
+
+*/
+
+console.log(myArr.indexOf(6));/*shows the location of the value inside the 
+                                array if it is available in the array but if it is
+                                not available in the array then it will give us -1 as 
+                                the output*/
+console.log(myArr.indexOf(5));
+
+
+
+const newArr = myArr.join()/*A string used to separate one element of the 
+                             array from the next in the resulting string. 
+                            If omitted, the array elements are separated with a comma.*/
+
+
+console.log(myArr); 
+console.log(newArr); 
+
+
+// Slice and Splice 
+
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("c", myn2);
+
+
+
+
+
+
