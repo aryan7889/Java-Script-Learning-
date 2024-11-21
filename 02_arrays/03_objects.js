e2dfb93cebdde920a46e1162ec53625fc66237b0
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-21 13:07:37 +0000

    hi

--- 02_arrays/03_objects.js
+++ 02_arrays/03_objects.js
@@ -1,17 +1,56 @@
-/*singleton
-object.create and isi ko bola jata hai constructor method ke thru 
-and isi se singleton banata hai.
+/*
+object ko declare krne ke do tareeke hote hai constructor ki tarah and
+literals ki tarah.*/
+
+
+
+/*singleton:
+koi bhi constructor se aap sabh bhi object banate hai toh ek singleton banta
+hai yaani singleton apne tareeke ka ek hi object hai.
+*/
+/*
+lekin jab hum dusre tareeke se banate hai toh tub multiple instance ban jate hai.
+and jab bhi literals ki tarah declare krte hai toh singleton nhi banta hai.
 */
 
+// object literals :::::::::::::::::
+ 
+// AND the curly braces we can see is only what we call as object.
 
-// object literals 
- const JsUser = {
+const mySym = Symbol("key1");
+
+const JsUser = {
     name: "Hitesh",
+    "fullname": "Hiteh Chaudhary",
+    [mySym]: "myKey1",
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["monday", "saturday"] 
  }
+
+//  How to acces object :
+console.log(JsUser.email);
+
+
+
+// console.log(JsUser[email]); we cant write it like this because it not defined 
+// as it is stored in object as string.
+
+
+// here is the correct methodd with the write syntax.
+console.log(JsUser["email"]);
+console.log(JsUser["fullname"]);
+console.log(JsUser[mySym]);
+
+
+JsUser.email = "Hiteh@chat.com"
+Object.freeze(JsUser)
+JsUser.email = "hitesh@micro.com"
+Object.freeze(JsUser)
+
+// console.log(JsUser);
+
 
  
