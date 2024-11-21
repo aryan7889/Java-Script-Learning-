6769946fa48b65b2392edb30fe297feddb479c8e
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-21 11:31:08 +0000

    arrays

--- 02_arrays/02_array.js
+++ 02_arrays/02_array.js
@@ -1,28 +1,38 @@
-const marvel_heros = ["thor", "Ironman", "spiderman"]
+const marvel_heros = ["thor", "Ironman", "spiderman"] /*Array has been declared */
 const dc_heros = ["superman", "flash", "batman"]
 
-marvel_heros.push(dc_heros)
+marvel_heros.push(dc_heros)/*Help us to push an array into another 
+                             array as array allow us to enter any datatype into 
+                             array so the output would be array ke andr array*/
 
 console.log(marvel_heros);
-console.log(marvel_heros[3][1]);
+console.log(marvel_heros[3][1]);/*Will give us the fouth value of the first
+                                  array and the second bracket show us the second 
+                                  value of second array.*/
 
-const allHeros = marvel_heros.concat(dc_heros)
+const allHeros = marvel_heros.concat(dc_heros)/*Will give us the the new array which 
+                                                is thw combination of the new array 
+                                                which include the elments of both the array 
+                                                in the form of new array/ */
 console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros]/*help us to spread the elements in the arrays*/
 
 console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity)/* by using flat and we have 
+                                                          to give the number into this and if 
+                                                          put infinity in it it will be solved till the 
+                                                          maximum.*/
 console.log(real_another_array);
 
 
 
 console.log(Array.isArray("Hitesh"))
 console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.from({name: "hitesh"})) //interesting
 
 let score1 = 100
 let score2 = 200
---
+++ 02_arrays/03_objects.js
@@ -0,0 +1,17 @@
+/*singleton
+object.create and isi ko bola jata hai constructor method ke thru 
+and isi se singleton banata hai.
+*/
+
+
+// object literals 
+ const JsUser = {
+    name: "Hitesh",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "saturday"] 
+ }
+
+ 
