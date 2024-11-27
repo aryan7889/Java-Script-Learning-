f1c286d0042d819a19689e4d3f672a6af4ddaa31
aryan7889 <aryan.goyalsons28@gmail.com>
2024-11-27 22:32:13 +0530

    objects

--- 02_arrays/04_objects.js
+++ 02_arrays/04_objects.js
@@ -26,12 +26,81 @@ const regularUser = {
         firstname: "hitesh",
         lastname: "chaudhary"}
    }
-
-
-
-
 }
+                                               /*here is the example of the syntax to access the nested object.*/
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname.userfullname.firstname);
 
-console.log(regularUser.fullname.userfullname.firstname);
-console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname?.userfullname.firstname); /* OPCTIONAL CHAIN: =>
+                      Agr hume pta nhi hai fullname object mei hei he nhi toh hum ise use kr skte hai api calling 
+                      ke time mei.*/
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { bj1, obj2 }
+            //     or 
+/*const obj3 = Object.assign({}, obj1, obj2, obj4)this syntax is generally 
+                                           used as a good syntax over the first one 
+                                           as here open parenthesis is the new empty object which will 
+                                           get all the value of the object that we want to and this is called source.*/
+// console.log(obj3);
+const obj3 = {...obj1,...obj2,...obj4}
+// console.log(obj3);
+
+
+const users = [
+{
+   id: 1,
+   email: "h@gmail.com",
+
+},
+{
+   id: 1,
+   email: "h@gmail.com",
+   
+},
+{
+   id: 1,
+   email: "h@gmail.com",
+   
+},
+{
+   id: 1,
+   email: "h@gmail.com",
+   
+},
+{
+   id: 1,
+   email: "h@gmail.com",
+   
+}
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
